96d33abe69974a3ee17b19aecac1f58ad9cfa5cc
Mahrozfatima786 <mahrozfatima156@gmail.com>
2025-10-22 21:00:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//your code here!
-
+const list = document.getElementById("infi-list");
+let itemCount = 1;
+function addItems(count) {
+  for (let i = 0; i < count; i++) {
+    const li = document.createElement("li");
+    li.textContent = `Item ${itemCount++}`;
+    list.appendChild(li);
+  }
+}
+addItems(10);
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight - 2) {
+    addItems(4); 
+  }
+});
